8b81cc174c17aa8842bd685001693f8c439d4f26
EpifaniusDaeli <warnaungu155@gmail.com>
2026-02-07 19:36:09 +0700

    perubahan isi file

---
+++ Pertemuan2/arrays.py
@@ -0,0 +1,14 @@
+hewan = ["kambing", "sapi", "kucing"]
+
+x = len(hewan)
+
+for x in hewan:
+  print(x)
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.append("orange")
+
+fruits = ['apple', 'banana', 'cherry']
+
+x = fruits.count("cherry")
+
--- biodata.md
+++
@@ -1,3 +0,0 @@
-nama : epifanius daeli
-nim : 25071207144
-prodi : s1 teknik informatika
