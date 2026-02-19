b51f3bb1e9e19203b0933bf8fe2396f9b5c37f57
EpifaniusDaeli <warnaungu155@gmail.com>
2026-02-19 22:11:23 +0700

    fix: convert materiPertemuan from submodule to normal folder

---
+++ materiPertemuan/Pertemuan2/ifElse.py
@@ -0,0 +1,73 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
+
---
+++ materiPertemuan/Pertemuan2/range.py
@@ -0,0 +1,6 @@
+for i in range(10):
+  print(i)
+
+print(list(range(5)))
+print(list(range(1, 6)))
+print(list(range(5, 20, 3)))
